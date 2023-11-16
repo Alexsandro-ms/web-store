56211bfb447c7c4e62c539514313ecf8fb7062d6
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-11-15 23:01:56 -0300

    feat: ensure user goes to catalog page on catalog button click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import { Card } from "./card";
 import { Button } from "./button";
 import { MenuIcon, ShoppingCartIcon, LogInIcon, HomeIcon, PercentIcon, ListOrderedIcon, LogOutIcon } from "lucide-react";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Separator } from "./separator";
+
+import Link from "next/link"
 
 const Header = () => {
     const { status, data } = useSession()
@@ -53,18 +55,26 @@ const Header = () => {
                      Fazer Login
                 </Button>
                )}
+               <Link href={"/"}>
+                <SheetClose asChild>
                 <Button variant={"outline"} className="w-full justify-start gap-2">
                     <HomeIcon size={16} />
                     Início
                 </Button>
+                </SheetClose>
+               </Link>
                 <Button variant={"outline"} className="w-full justify-start gap-2">
                     <PercentIcon size={16} />
                     Ofertas
                 </Button>
+               <SheetClose asChild>
+               <Link href={"/catalog"}>
                 <Button variant={"outline"} className="w-full justify-start gap-2">
                     <ListOrderedIcon size={16} />
                     Catálogo
                 </Button>
+                </Link>
+               </SheetClose>
                 {status === "authenticated" && (
                     <Button onClick={handleSignOutClick} variant={"outline"} className="w-full justify-start gap-2">
                      <LogOutIcon size={16} />
