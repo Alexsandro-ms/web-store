cf0e865b51d4d1f4f52c6ba109fcf0b381d16ac8
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-11-19 15:26:52 -0300

    feat: add cart item component and improved cart product addition

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,24 +1,31 @@
 "use client";
 
 import { useContext } from "react";
-import { ShapesIcon } from "lucide-react";
+import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { CartContext } from "@/provider/cart";
+import CartItem from "./cart-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 const Cart = () => {
   const { products } = useContext(CartContext);
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant={"outline"}
       >
-        <ShapesIcon size={16} />
+        <ShoppingCartIcon size={16} />
         Carrinho
       </Badge>
-      {products.map((product) => (
-        <h1 key={product.id}>{product.name}</h1>
-      ))}
+      <div className="flex flex-col gap-5">
+        {products.map((product) => (
+          <CartItem
+            key={product.id}
+            product={computeProductTotalPrice(product as any) as any}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/provider/cart.tsx
+++ src/provider/cart.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useState } from "react";
-import { Product } from "@prisma/client";
 import { ReactNode, createContext } from "react";
+import { ProductWithTotalPrice } from "@/helpers/product";
 
-interface CartProduct extends Product {
-  quantity: Number;
+export interface CartProduct extends ProductWithTotalPrice {
+  quantity: number;
 }
 
 interface ICartContext {
@@ -26,7 +26,34 @@ export const CartContext = createContext<ICartContext>({
 const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
 
+  // Função que lida com a adição de um produto ao carrinho
   const handleAddProductToCart = (product: CartProduct) => {
+    // Verifica se o produto já está no carrinho
+    const productIsAlreadyCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    // Se o produto já estiver no carrinho
+    if (productIsAlreadyCart) {
+      // Atualiza a lista de produtos no estado, incrementando a quantidade do produto existente
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          // Verifica se o produto no carrinho é o mesmo que está sendo adicionado
+          if (cartProduct.id === product.id) {
+            // Retorna uma nova versão do produto no carrinho com a quantidade atualizada
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            };
+          }
+
+          // Se não for o produto que está sendo adicionado, retorna o produto inalterado
+          return cartProduct;
+        }),
+      );
+      return;
+    }
+    // Se o produto ainda não estiver no carrinho, adiciona-o à lista de produtos no estado
     setProducts((prev) => [...prev, product]);
   };
 
