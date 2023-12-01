ce833fa42e6a6e1b359fdbd4e1fb2635983545ca
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-12-01 12:25:16 -0300

    style: created responsiveness for the product page desktop

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -30,7 +30,7 @@ const ProductDetailsPage = async ({
   if (!product) return null;
 
   return (
-    <div className="flex flex-col gap-8 pb-8">
+    <div className="flex flex-col gap-8 pb-8 lg:grid lg:auto-cols-min">
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
       <ProductInfo product={computeProductTotalPrice(product)} />
       <div>
