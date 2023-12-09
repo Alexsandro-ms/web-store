aeaaf8beb3d7b7d2c4f33bcca0cdb2481b82214a
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-12-09 11:36:48 -0300

    feat: add loading component in the pages

---
+++ src/app/deals/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <h1 className="font-bold">Carregando...</h1>
+    </div>
+  );
+};
+
+export default Loading;
