d81f0364aa49734ba9850d38554d4b1bc86e40bf
Alexsandro Martins <alexsandromartins.santo@hotmail.com>
2024-01-31 16:25:18 -0300

    feat: new cart ui

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -26,13 +26,7 @@ const Cart = () => {
 
   return (
     <div className="flex h-full flex-col gap-8">
-      <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
-        variant={"outline"}
-      >
-        <ShoppingCartIcon size={16} />
-        Carrinho
-      </Badge>
+      <p className="font-bold underline text-primary">Carrinho</p>
       <div className="flex h-full flex-col overflow-hidden">
         <ScrollArea className="h-full">
           <div className="flex h-full flex-col gap-8">
